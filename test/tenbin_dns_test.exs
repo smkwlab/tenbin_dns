9944171ac13d57024e94f6588a0897757e4e364b
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2021-08-14 19:27:58 +0900

    correct typo

--- test/tenbin_dns_test.exs
+++ test/tenbin_dns_test.exs
@@ -7,7 +7,7 @@ defmodule TenbinDnsTest do
     assert TenbinDns.hello() == :world
   end
 
-  test "creqte_domain_name" do
+  test "create_domain_name" do
     assert DNSpacket.create_domain_name("example.com.") == <<7>><>"example"<><<3>><>"com"<><<0>>
   end
 
