b1541d9c09b20cd9e0e1e675126c82b72980eba9
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2024-01-18 00:26:45 +0900

    update version 0.3.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TenbinDns.MixProject do
   def project do
     [
       app: :tenbin_dns,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
