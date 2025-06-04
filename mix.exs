7d20dc721fec2a338f321992bba499c19e94cfc4
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2025-06-04 18:51:32 +0900

    Bump version to 0.5.4 for performance optimization release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TenbinDns.MixProject do
   def project do
     [
       app: :tenbin_dns,
-      version: "0.5.3",
+      version: "0.5.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
